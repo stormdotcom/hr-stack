25a2895ed771d810c12ad73873fad2d19d5f341c
Ajmal <ajmaln73@gmail.com>
2022-02-15 11:30:37 +0530

    server 2

---
+++ server/routes/server2.js
@@ -0,0 +1,56 @@
+import createError from 'http-errors';
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import cors from 'cors'
+import employeeRouter from './routes/employee.js';
+import hrRouter from './routes/hr.js';
+import adminRouter from "./routes/admin.js"
+import mongoose from "mongoose"
+import http from "http";
+import { createServer } from "http";
+import { Server } from "socket.io";
+const app = express();
+const CONNECTION_URL = "mongodb://localhost:27017/hr-management";
+const port = 4001;
+
+app.use(cors())
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', employeeRouter);
+app.use('/hr', hrRouter);
+app.use('/admin', adminRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.json({ message: err.message});
+});
+mongoose.connect(CONNECTION_URL)
+.then(()=>{ console.log("DB Connected"); 
+})
+.catch((err)=> console.log("Error connecting DB \n" + err.message))
+const server = http.createServer(app);
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, { 
+  cors:{
+    origin:['http://localhost:3000']
+  }
+
+});
+
+httpServer.listen(port, ()=> console.log("server running on port " + port));
