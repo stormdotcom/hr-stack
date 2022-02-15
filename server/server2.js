83d7c6f671dc3b7488cc244112f7f7b0fcab6864
Ajmal <ajmaln73@gmail.com>
2022-02-15 12:31:14 +0530

    port setted

--- server/server2.js
+++ server/server2.js
@@ -8,8 +8,6 @@ import hrRouter from './routes/hr.js';
 import adminRouter from "./routes/admin.js"
 import mongoose from "mongoose"
 import http from "http";
-import { createServer } from "http";
-import { Server } from "socket.io";
 const app = express();
 const CONNECTION_URL = "mongodb://localhost:27017/hr-management";
 const port = 4001;
