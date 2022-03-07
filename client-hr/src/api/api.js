6e6cb553842d862fb5b5d9af6f782d0c7ca472f8
Ajmal <ajmaln73@gmail.com>
2022-03-07 20:16:42 +0530

    final final tuning api

--- client-hr/src/api/api.js
+++ client-hr/src/api/api.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 const API = axios.create({ baseURL: 'https://api-hr-stack.xyz' });
 
+// Local development
 // const API = axios.create({ baseURL: 'http://localhost:4000' });
 
 
--- client/src/api/employee.js
+++ client/src/api/employee.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 const API = axios.create({ baseURL: 'https://api-hr-stack.xyz' });
 
+// Local development
 // const API = axios.create({ baseURL: 'http://localhost:4000' });
 export const signIn = (formData) => API.post('/signin', formData);
 export const fetchEmployeeData = (id) => API.get('/fetchEmployeeData?id='+id);
